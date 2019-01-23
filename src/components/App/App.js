19bdf45a6daaabadb073df5b1f3b9388fa931c38
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-01-22 20:05:28 -0600

    update app.js with styling and component imports

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,42 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import CountrySelect from "../CountrySelect/CountrySelect";
+import "./App.css";
+import 'typeface-roboto';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Navbar from '../Navbar/Navbar'
+import Grid from '@material-ui/core/Grid';
+
+const styles = {
+  root: {
+    width: '100%',
+    maxWidth: 500,
+  },
+};
 
 class App extends Component {
+
+
   render() {
+    // declared for later styling of content
+    // const { classes } = this.props;
+
+
     return (
       <div>
+        <Navbar />
+        <Grid container spacing={12}>
+        <Grid item large>
+          <CountrySelect />
+        </Grid>
 
+        </Grid>
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+export default withStyles(styles)(App);
