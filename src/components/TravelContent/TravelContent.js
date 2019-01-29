0d0c11a9d2e86b0ba8811ed79ba048c61adb4daf
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-01-29 10:15:20 -0600

    change vmin to vh for scaling in mobile and add mat ui Card for country name and flag display

--- src/components/TravelContent/TravelContent.js
+++ src/components/TravelContent/TravelContent.js
@@ -8,15 +8,24 @@ import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
 
 // jss styles
 const styles = theme => ({
-  card: {
-    paddingBottom: "2vmin"
-  },
   heading: {
     fontSize: theme.typography.pxToRem(15),
     fontWeight: theme.typography.fontWeightRegular
+  },
+  media: {
+    height: "20vh",
+    
+  },
+  card: {
+    width: "40vh",
+    height: "30vh",
+    textAlign: "center"
   }
 });
 
@@ -35,11 +44,15 @@ class TravelContent extends Component {
           {this.props.countryInfo.map((info, index) => (
             <div key={index}>
 
-             <Typography variant="h4" className="countryName">
-                <b>{info.basic.name.common}</b>
-             </Typography>
-
-             <br></br>
+              <Card className={classes.card} key={index}>
+                <CardMedia
+                  className={classes.media}
+                  image={info.basic.flag.png}
+                />
+                <CardContent>
+                  <Typography variant="h5">{info.basic.name.common}</Typography>
+                </CardContent>
+              </Card>
 
               {/* basics */}
               <ExpansionPanel>
@@ -50,16 +63,14 @@ class TravelContent extends Component {
                 </ExpansionPanelSummary>
                 <ExpansionPanelDetails>
                   <Typography component={"div"} variant={"body2"}>
-                  <p>
-                    <strong>Capital</strong>: &nbsp;
-                    <span>{info.basic.capital.name}</span>
-                  </p>
-                  <p>
-                    <strong>Capital</strong>: &nbsp;
-                    <span>{info.basic.capital.name}</span>
-                  </p>
-                      
-               
+                    <p>
+                      <strong>Capital</strong>: &nbsp;
+                      <span>{info.basic.capital.name}</span>
+                    </p>
+                    <p>
+                      <strong>Currency</strong>: &nbsp;
+                      <span>{info.basic.capital.name}</span>
+                    </p>
                   </Typography>
                 </ExpansionPanelDetails>
               </ExpansionPanel>
